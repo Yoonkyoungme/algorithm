82946559bd6417773eb4d417a9a59a58a13c930b
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2023-01-22 22:44:45 +0900

    [level 1] Title: 약수의 합, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,11 +1,11 @@
 function solution(n) {
     let count = 0;
     
-    for(let i = 1; i <= n; i++){
+    for(let i = 1; i <= n/2; i++){
         if (n % i === 0){
             count += i;
         }
     }
     
-    return count;
+    return count + n;
 }
