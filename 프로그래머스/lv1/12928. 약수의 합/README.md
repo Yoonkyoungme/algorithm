a65578360963abb1d995972537a16333dbef1c50
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2023-01-22 23:28:40 +0900

    [level 1] Title: 약수의 합, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/README.md
+++ 프로그래머스/lv1/12928. 약수의 합/README.md
@@ -1,10 +1,10 @@
 # [level 1] 약수의 합 - 12928 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12928) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12928#) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.16 ms
+메모리: 33.5 MB, 시간: 0.11 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,11 +1,14 @@
 function solution(n) {
-    let count = 0;
-    
-    for(let i = 1; i <= n/2; i++){
-        if (n % i === 0){
-            count += i;
-        }
+  let count = 0;
+
+  for (let i = 1; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      count += i;
+
+      let num = n / i;
+      if (num !== i) count += num;
     }
-    
-    return count + n;
+  }
+
+  return count;
 }
