7289e11fde023cf31735a7c9a07659e53a830763
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2023-02-20 00:13:40 +0900

    refactor: 주유소.js

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -5,21 +5,22 @@ const input = require("fs")
   .split("\n");
 
 const N = +input[0];
+
 const cityLength = input[1].split(" ").map(Number);
 let totalLength = cityLength.reduce((acc, cur) => acc + cur);
 const price = input[2].split(" ").map(Number).slice(0, -1);
-let result = 0;
+let min = price[0];
+let result = BigInt(min * cityLength[0]);
+totalLength -= cityLength[0];
 
-for (let i = 0; i < N - 1; i++) {
-  const min = Math.min(...price);
+for (let i = 1; i < N - 1; i++) {
+  if (totalLength === 0) break;
 
-  if (price[i] === min) {
-    result += min * totalLength;
-    break;
-  } else {
-    result += price[i] * cityLength[i];
-    totalLength -= cityLength[i];
+  if (price[i] < min) {
+    min = price[i];
   }
+  result += BigInt(min * cityLength[i]);
+  totalLength -= cityLength[i];
 }
 
-console.log(result);
+console.log(String(result));
