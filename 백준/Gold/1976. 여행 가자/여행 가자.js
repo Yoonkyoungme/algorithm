57847c7a6b0ca9f38da15a6a8b9f373993454c4c
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2023-02-20 19:45:40 +0900

    [Gold IV] Title: 여행 가자, Time: 184 ms, Memory: 10932 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.js
@@ -0,0 +1,49 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const N = +input.shift();
+const M = +input.shift();
+const plan = input.pop().split(" ").map(Number);
+const graph = input.map((data) => data.split(" ").map(Number));
+
+let parents = [];
+// 초기화: 자기 자신의 값을 부모로 가지는 배열 생성
+for (let i = 0; i < N; i++) parents.push(i);
+
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < N; j++) {
+    if (graph[i][j] === 1) union(i, j);
+  }
+}
+
+for (let i = 1; i < M; i++) {
+  if (!findParent(plan[i - 1] - 1, plan[i] - 1)) {
+    console.log("NO");
+    return;
+  }
+}
+
+console.log("YES");
+
+function getParent(x) {
+  if (parents[x] === x) return x;
+  return (parents[x] = getParent(parents[x]));
+}
+
+function union(x, y) {
+  const n1 = getParent(x);
+  const n2 = getParent(y);
+
+  n1 < n2 ? (parents[n2] = n1) : (parents[n1] = n2);
+}
+
+function findParent(x, y) {
+  const n1 = getParent(x);
+  const n2 = getParent(y);
+
+  if (n1 === n2) return true;
+  else return false;
+}
