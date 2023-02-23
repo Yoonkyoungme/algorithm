89116ec3f5a799ea533e58a425b98bd84e8d6c00
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2023-02-23 23:07:28 +0900

    [level 3] Title: 여행경로, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.js
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.js
@@ -1,24 +1,25 @@
 function solution(tickets) {
-  let visited = new Array(tickets.length).fill(false);
-  let result = [];
+    let result = [];
+    let visited = new Array(tickets.length).fill(false);
+    
+    tickets.sort();
 
-  tickets.sort();
-
-  function dfs(cur, count, path) {
-    if (count === tickets.length && result.length === 0) {
-      result = path;
-      return;
-    }
-
-    for (let i = 0; i < tickets.length; i++) {
-      if (!visited[i] && tickets[i][0] === cur) {
-        visited[i] = true;
-        dfs(tickets[i][1], count + 1, [...path, tickets[i][1]]);
-        visited[i] = false;
-      }
-    }
-  }
-
-  dfs("ICN", 0, ["ICN"]);
-  return result;
+    function dfs(path) {
+        if (path.length === tickets.length + 1) {
+            result.push(path)
+            return;
+        }
+        
+        for (let i = 0; i < tickets.length; i++) {
+                if(!visited[i] && tickets[i][0] === path[path.length - 1]){
+                    visited[i] = true;
+                    dfs([...path, tickets[i][1]]);
+                    visited[i] = false;
+                }
+            }
+        }
+    
+    dfs(["ICN"]);
+    
+    return result[0];
 }
