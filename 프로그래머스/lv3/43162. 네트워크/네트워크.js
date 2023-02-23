5dafdae5f609a8c33a6c4fd888949e69aa288ce8
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2023-02-23 17:18:53 +0900

    [level 3] Title: 네트워크, Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.js
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.js
@@ -1,26 +1,21 @@
 function solution(n, computers) {
-    let result = 0;
-    let visited = new Array(n).fill(false);
-    
-    /**
-    방문하지 않은 now node를 탐색한다.
-    탐색할 node는 이어져있고, 방문한적 없는 노드이다.
-    */
-    function searchNextNode(now){
-        visited[now] = true;
-        for (let next = 0 ; next < n; next++){
-            if (computers[now][next] === 1 && !visited[next]){
-                searchNextNode(next);               
-            }    
-        }
+  let result = 0;
+  let visited = new Array(n).fill(false);
+
+  function dfs(now) {
+    visited[now] = true;
+
+    for (let next = 0; next < n; next++) {
+      if (!visited[next] && computers[now][next] === 1) dfs(next);
     }
-    
-    for (let i = 0; i < n; i++){
-        if (!visited[i]){
-            visited[i] = true;
-            searchNextNode(i);
-            result += 1;        
-        }  
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (!visited[i]) {
+      dfs(i);
+      result += 1;
     }
-    return result;
+  }
+
+  return result;
 }
