95b5ca17d418a15c8bab5c48858cff2baae17b43
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2023-02-25 13:15:39 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.69 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,11 +1,12 @@
 function solution(maps) {
-  const visited = new Array(maps.length)
+  const dist = new Array(maps.length)
     .fill(0)
-    .map(() => new Array(maps[0].length).fill(1));
+    .map(() => new Array(maps[0].length).fill(-1));
   const dx = [0, 1, 0, -1];
   const dy = [-1, 0, 1, 0];
 
   const queue = [[0, 0]];
+  dist[0][0] = 1;
 
   while (queue.length) {
     const [y, x] = queue.shift();
@@ -19,15 +20,16 @@ function solution(maps) {
         nx >= 0 &&
         ny < maps.length &&
         nx < maps[0].length &&
-        maps[ny][nx] === 1
+        maps[ny][nx] === 1 &&
+        dist[ny][nx] === -1
       ) {
-        {
-          visited[ny][nx] = visited[y][y] + 1;
-          queue.push([ny, nx]);
-        }
+        dist[ny][nx] = dist[y][x] + 1;
+        queue.push([ny, nx]);
       }
     }
   }
 
-  return visited[-1][-1] === 1 ? -1 : visited[-1][-1];
+  return dist[maps.length - 1][maps[0].length - 1] === -1
+    ? -1
+    : dist[maps.length - 1][maps[0].length - 1];
 }
