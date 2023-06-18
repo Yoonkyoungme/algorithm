3930b386f5d14b0c075aba2f827a6b85acbf1a42
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2023-06-18 22:04:28 +0900

    [level 1] Title: 2016년, Time: 0.33 ms, Memory: 34.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,7 +1,4 @@
 function solution(a, b) {
-    const week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
     const date = new Date(`2016-${a}-${b}`);
-    const day = date.getDay();
-    
-    return week[day];
+    return date.toString().slice(0, 3).toUpperCase();
 }
