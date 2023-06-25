46be83199e1cdd22e20f4d3eccdd1aafd21c9f03
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2023-06-25 22:43:58 +0900

    [level 2] Title: 숫자의 표현, Time: 0.32 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,18 +1,26 @@
 function solution(n) {
-    let result = 0;
-    
-    for (let i = 0; i < n - 1; i++) {
-        let count = 0;
-        for (let j = i + 1; j < n + 1; j++) {
-            if (count > n) {
-                break
-            }
-            else if (count === n) {
-                result ++;
-                break
-            }
-            count += j;
-        }
+  // 방법1
+  let result = 0;
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0 && i % 2 === 1) {
+      result++;
     }
-    return result + 1
+  }
+  return result;
+
+  // 방법2
+  // let result = 0;
+  // for (let i = 0; i < n - 1; i++) {
+  //   let count = 0;
+  //   for (let j = i + 1; j < n + 1; j++) {
+  //     if (count > n) {
+  //       break;
+  //     } else if (count === n) {
+  //       result++;
+  //       break;
+  //     }
+  //     count += j;
+  //   }
+  // }
+  // return result + 1;
 }
