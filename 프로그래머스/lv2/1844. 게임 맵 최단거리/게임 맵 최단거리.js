4a70d0be3f37702d18e6c23411b81adc7c840f8e
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2023-07-03 19:21:56 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 26.70 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,35 +1,37 @@
 function solution(maps) {
-  const dist = new Array(maps.length)
-    .fill(0)
-    .map(() => new Array(maps[0].length).fill(-1));
-  const dx = [0, 1, 0, -1];
-  const dy = [-1, 0, 1, 0];
-
+  const height = maps.length;
+  const width = maps[0].length;
+  const dx = [1, -1, 0, 0];
+  const dy = [0, 0, 1, -1];
   const queue = [[0, 0]];
+
+  const dist = new Array(height).fill().map(() => Array(width).fill(-1));
   dist[0][0] = 1;
 
-  while (queue.length) {
+  function isValidCoordinate(x, y) {
+    return (
+      x >= 0 &&
+      y >= 0 &&
+      x < width &&
+      y < height &&
+      dist[y][x] === -1 &&
+      maps[y][x] === 1
+    );
+  }
+
+  while (queue.length > 0) {
     const [y, x] = queue.shift();
 
     for (let i = 0; i < 4; i++) {
-      let ny = y + dy[i];
-      let nx = x + dx[i];
+      const nx = x + dx[i];
+      const ny = y + dy[i];
 
-      if (
-        ny >= 0 &&
-        nx >= 0 &&
-        ny < maps.length &&
-        nx < maps[0].length &&
-        maps[ny][nx] === 1 &&
-        dist[ny][nx] === -1
-      ) {
+      if (isValidCoordinate(nx, ny)) {
         dist[ny][nx] = dist[y][x] + 1;
         queue.push([ny, nx]);
       }
     }
   }
 
-  return dist[maps.length - 1][maps[0].length - 1] === -1
-    ? -1
-    : dist[maps.length - 1][maps[0].length - 1];
+  return dist[height - 1][width - 1] === -1 ? -1 : dist[height - 1][width - 1];
 }
