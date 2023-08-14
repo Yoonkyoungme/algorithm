7860fc5d0da1ba58e8506949f2cc5ba62da2aa0a
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2023-08-14 23:30:21 +0900

    [level 2] Title: H-Index, Time: 0.06 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -0,0 +1,10 @@
+function solution(citations) {
+    citations.sort((a, b) => b - a);
+    
+    for (let i = 0; i < citations.length; i++) {
+        if (citations[i] <= i) {
+            return i;
+        }
+    }
+    return citations.length;
+}
