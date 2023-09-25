23b1a180d21438fbb3911d37932c48029db04082
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2023-09-25 16:00:04 +0900

    [level 2] Title: 프로세스, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.js
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.js
@@ -16,3 +16,62 @@ function solution(priorities, location) {
 
   return result;
 }
+
+
+// class Node {
+//   constructor(value) {
+//       this.value = value;
+//       this.next = null;
+//   }
+// }
+
+// class Queue {
+//   constructor() {
+//       this.head = null;
+//       this.tail = null;
+//   }
+
+//   enqueue(newValue) {
+//       const newNode = new Node(newValue);
+//       if (this.head === null) {
+//           this.head = this.tail = newNode;
+//       } else {
+//           this.tail.next = newNode;
+//           this.tail = newNode;
+//       }
+//   }
+
+//   dequeue() {
+//       const value = this.head.value;
+//       this.head = this.head.next;
+//       return value;
+//   }
+
+//   peek() {
+//       return this.head.value;
+//   }
+// }
+
+// function solution(priorities, location) {
+//   const queue = new Queue();
+
+//   for (let i = 0; i < priorities.length; i++) {
+//       queue.enqueue([priorities[i], i]);
+//   }
+
+//   priorities.sort((a, b) => b - a);
+  
+//   let count = 0;
+//   while(true) {
+//       const currentValue = queue.peek();
+//       if (currentValue[0] < priorities[count]) {
+//           queue.enqueue(queue.dequeue());
+//       } else {
+//           const value = queue.dequeue();
+//           count++;
+//           if (value[1] === location) {
+//               return count;
+//           }
+//       }
+//   }
+// }
