2a4a5466f8b6ab5c5756fdd37de47cae6d9fd961
Yoonkyoung (빙봉) <dev.yoonkyoung@gmail.com>
2024-11-03 01:36:24 +0900

    [level 2] Title: 예상 대진표, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,26 @@
+function solution(n,a,b) {
+    let result = 0;
+    
+    while (a !== b) {
+        a = Math.ceil(a / 2);
+        b = Math.ceil(b / 2);
+        
+        result++;
+    }
+    
+    return result;
+}
+
+
+
+
+// function solution(n, a, b) {
+//   let result = 0;
+
+//   while (Math.ceil(a) !== Math.ceil(b)) {
+//     a = a / 2;
+//     b = b / 2;
+//     result += 1;
+//   }
+//   return result;
+// }
