d7439ac2a59353e5f3898d45aa386433e3fa6cd4
Yoonkyoung (빙봉) <dev.yoonkyoung@gmail.com>
2024-11-02 19:47:10 +0900

    [level 2] Title: 주식가격, Time: 20.97 ms, Memory: 44.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,15 +1,20 @@
 function solution(prices) {
     const totalPriceCount = prices.length;
     const results = Array.from({length: totalPriceCount}).fill(0);
-    
-    for (let i = 0; i < totalPriceCount; i++) {        
-        for (let j = i + 1; j < totalPriceCount; j++) {
-            results[i]++;
-            if (prices[i] > prices[j]) {
-                break;
-            }
-        }    
+    const stack = [];
+
+    for (let i = 0; i < totalPriceCount; i++) {
+        while (stack.length > 0 && prices[i] < prices[stack.at(-1)]) {
+            const j = stack.pop();
+            results[j] = i - j;
+        }
+        stack.push(i);
     }
-    
+
+    while (stack.length > 0) {
+        const j = stack.pop();
+        results[j] = totalPriceCount - 1 - j;
+    }
+
     return results;
 }
