16dddc9a4f1bd7d49d7042d64c79b776a78817c1
Yoonkyoung (빙봉) <dev.yoonkyoung@gmail.com>
2024-11-08 01:32:30 +0900

    Time: 0 ms (100%), Space: 49.2 MB (62.96%) - LeetHub

--- LeetCode/0014-longest-common-prefix/0014-longest-common-prefix.js
+++ LeetCode/0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,49 +3,16 @@
  * @return {string}
  */
 
-function findPrefixes(str) {
-    let currentPrefix = "";
-    const prefixes = [];
-
-    for (let i = 0; i < str.length; i++) {
-        currentPrefix += str[i]; 
-        prefixes.push(currentPrefix); 
-    }
-
-    return prefixes;
-}
-
-
 const longestCommonPrefix = function(strs) {
-    
-    // 1. find all prefixes
-    let allPrefixes = [];
+    let prefix = strs[0];
 
-    for (let i = 0; i < strs.length; i++) {
-        const prefixes = findPrefixes(strs[i]);
-        allPrefixes = [...allPrefixes, ...prefixes];
-    }
+    for (let i = 1; i < strs.length; i++) {
+        while (!strs[i].startsWith(prefix)) {
+            prefix = prefix.slice(0, -1);
 
-    allPrefixes = [...new Set(allPrefixes)];
-
-    // 2. find common prefix
-    const commoonPrefixs = [];
-
-    allPrefixes.forEach((prefix) => {
-        const isPrefixCommon = strs.every((str) => str.startsWith(prefix));
-
-        if (isPrefixCommon) {
-            commoonPrefixs.push(prefix);
+            if (prefix === "") return "";
         }
-    })
-
-
-
-    // 3. find longest common prefix
-    if (commoonPrefixs.length === 0) {
-        return "";
     }
 
-    const results = commoonPrefixs.sort(); // ascending order
-    return results.at(-1);
+    return prefix;
 };
