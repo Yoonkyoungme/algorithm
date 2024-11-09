0d97b903bc70fc1a860ef6af0e7f8eccba5a2415
Yoonkyoung (빙봉) <dev.yoonkyoung@gmail.com>
2024-11-09 13:45:15 +0900

    Time: 0 ms (100%), Space: 48.5 MB (85.41%) - LeetHub

--- LeetCode/0058-length-of-last-word/0058-length-of-last-word.js
+++ LeetCode/0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,13 +3,7 @@
  * @return {number} - the length of the last word in the string
  */
 const lengthOfLastWord = function(s) {
-    // Step 1: Trim any trailing spaces from the string
-    s = s.trim();
+    const words = s.trim().split(' ');
 
-    // Step 2: Find the last space in the trimmed string
-    const lastSpaceIndex = s.lastIndexOf(' ');
-    console.log(s, s.length, lastSpaceIndex)
-
-    // Step 3: The last word is the substring after the last space, so calculate its length
-    return s.length - lastSpaceIndex - 1;
+    return words[words.length - 1].length;
 };
