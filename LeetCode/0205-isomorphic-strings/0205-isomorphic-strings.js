5128c2f1dc755943f61e8a628a06ce179c13df07
Yoonkyoung (빙봉) <dev.yoonkyoung@gmail.com>
2024-11-10 18:09:43 +0900

    Time: 11 ms (26.16%), Space: 52.8 MB (19.16%) - LeetHub

--- LeetCode/0205-isomorphic-strings/0205-isomorphic-strings.js
+++ LeetCode/0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -4,16 +4,28 @@
  * @return {boolean}
  */
 const isIsomorphic = function(s, t) {
-    const alphabetMap = new Map();
+    const sMap = new Map();
+    const tMap = new Map();
 
     for (let i = 0; i < s.length; i++) {
-        if (alphabetMap.has(s[i])) {
-            const value = alphabetMap.get(s[i]);
+        if (sMap.has(s[i])) {
+            const value = sMap.get(s[i]);
             if (value !== t[i]) {
                 return false;
             }
         } else {
-            alphabetMap.set(s[i], t[i]);
+            sMap.set(s[i], t[i]);
+        }
+    }
+
+    for (let i = 0; i < t.length; i++) {
+        if (tMap.has(t[i])) {
+            const value = tMap.get(t[i]);
+            if (value !== s[i]) {
+                return false;
+            }
+        } else {
+            tMap.set(t[i], s[i]);
         }
     }
 
