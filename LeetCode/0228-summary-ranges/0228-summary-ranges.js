667f6196d93e14340f3cb6180d7870ea158779c6
Yoonkyoung (빙봉) <dev.yoonkyoung@gmail.com>
2024-11-12 15:29:22 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/0228-summary-ranges/0228-summary-ranges.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+const summaryRanges = function(nums) {
+    const results = [];
+    let startNum = null;
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+
+        if (start === null) {
+            startNum = num
+        }
+
+        if (i === nums.length - 1 || num + 1 !== nums[i + 1]) {
+            if (start === num) {
+                results.push(`${startNum}`);
+            } else {
+                results.push(`${startNum}->${num}`);
+            }
+            startNum = null;
+        }
+    }
+
+    return results;
+};
