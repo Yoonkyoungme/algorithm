b89890517a879ce0e31013cbbf7baff0d49735aa
Yoonkyoung (빙봉) <dev.yoonkyoung@gmail.com>
2024-11-12 22:31:51 +0900

    Time: 2 ms (74.81%), Space: 51.5 MB (26.15%) - LeetHub

--- LeetCode/0001-two-sum/0001-two-sum.js
+++ LeetCode/0001-two-sum/0001-two-sum.js
@@ -5,12 +5,12 @@
  */
 const twoSum = function(nums, target) {
     const map = new Map();
-    
-    for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
 
-        if (map.has(complement)) {
-            return [map.get(complement), i];
+    for (let i = 0; i < nums.length; i++) {
+        const subtractedNum = target - nums[i];
+
+        if (map.has(subtractedNum)) {
+            return [map.get(subtractedNum), i];
         }
 
         map.set(nums[i], i);
